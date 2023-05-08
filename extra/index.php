49f672aa410cbe0f86a974662a817f1b7ba65393
Seantheman01 <sean.v.g@outlook.com>
2023-05-08 10:30:07 +0200

    committed

--- extra/index.php
+++ extra/index.php
@@ -3,9 +3,9 @@
 <body>
     
 <form action="actie.php" method="post">
-    <p>Naam: <input type = "text" name = "name"/></p>
-    <p>Leeftijd: <input type = "text" name = "age"/></p>
-    <p>Verzenden: <input type = "send"/></p>
+<p>Naam: <input type = "text" name = "name"/></p>
+<p>Leeftijd: <input type = "text" name = "age"/></p>
+<p><input type = "submit"/></p>
 </form>
 
 </body>
