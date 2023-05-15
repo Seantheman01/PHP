98bdd8930b8d750c5d9bf3fb9d6b21f9363c993c
Seantheman01 <sean.v.g@outlook.com>
2023-05-15 11:23:46 +0200

    committed

--- De_Basics_of_Back-end/Back-end_1/BE1O3/index.php
+++ De_Basics_of_Back-end/Back-end_1/BE1O3/index.php
@@ -6,4 +6,9 @@ $getal2 = rand(1, 10);
 echo("Het eerste getal is: ". $getal1 . "<br>");
 echo("Het tweede getal is: ". $getal2 . "<br>");
 
+$x = 20;  
+$x += 100;
+
+echo $x;
+
 ?>
--- index.php
+++ index.php
@@ -6,4 +6,9 @@ $getal2 = rand(1, 10);
 echo("Het eerste getal is: ". $getal1 . "<br>");
 echo("Het tweede getal is: ". $getal2 . "<br>");
 
+$x = 20;  
+$getal1 += $getal2;
+
+echo $getal1;
+
 ?>
