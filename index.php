3baed4d5488ffa45c8fa29a3492bc73f8845442b
Seantheman01 <sean.v.g@outlook.com>
2023-05-15 10:24:54 +0200

    committed

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php echo "<h1>Hello world!</h1>";
+<?php
+
+$getal1 = rand(1, 10);
+$getal2 = rand(1, 10);
+
+echo("Het eerste getal is: ". $getal1 . "<br>");
+echo("Het tweede getal is: ". $getal2 . "<br>");
+
 ?>
