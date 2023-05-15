dcdbbf820a3b48505e7b5becaab67b3518e28ea1
Seantheman01 <sean.v.g@outlook.com>
2023-05-15 13:37:50 +0200

    committed

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-
-$getal1 = mt_rand(1, 10);
-$getal2 = mt_rand(1, 10);
-
-echo $getal1 ." + ".  $getal2 ." = ". $getal1 + $getal2 ."<br>";
-echo $getal1 ." - ".  $getal2 ." = ". $getal1 - $getal2 ."<br>";
-echo $getal1 ." x ".  $getal2 ." = ". $getal1 * $getal2 ."<br>";
-echo $getal1 ." : ".  $getal2 ." = ". $getal1 / $getal2 ."<br>";
-
+echo date("h:i:s") . "<br>";
 ?>
