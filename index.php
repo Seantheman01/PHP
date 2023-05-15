f9ca98cea4aee92e85e0b7fe26a8c56ce7e40471
Seantheman01 <sean.v.g@outlook.com>
2023-05-15 11:49:52 +0200

    committed

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-$getal1 = rand(1, 10);
-$getal2 = rand(1, 10);
+$getal1 = mt_rand(1, 10);
+$getal2 = mt_rand(1, 10);
 
-echo($getal1 ." + ".  $getal2 ." = ". $getal1 += $getal2 ."<br>");
-echo($getal1 ." - ".  $getal2 ." = ". $getal1 -= $getal2 ."<br>");
-echo($getal1 ." x ".  $getal2 ." = ". $getal1 *= $getal2 ."<br>");
-echo($getal1 ." : ".  $getal2 ." = ". $getal1 /= $getal2 ."<br>");
+echo $getal1 ." + ".  $getal2 ." = ". $getal1 + $getal2 ."<br>";
+echo $getal1 ." - ".  $getal2 ." = ". $getal1 - $getal2 ."<br>";
+echo $getal1 ." x ".  $getal2 ." = ". $getal1 * $getal2 ."<br>";
+echo $getal1 ." : ".  $getal2 ." = ". $getal1 / $getal2 ."<br>";
 
 ?>
