acfaa1996924eb761c6f264ed022b7013a90d070
Seantheman01 <sean.v.g@outlook.com>
2023-05-22 11:13:47 +0200

    committed

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 </head>
 <?php
     date_default_timezone_set("Europe/Amsterdam");
-    echo 
-    date("h:i:s");
+    echo date("h:i:s");
     $plaatje = "morning.png";
     ?>
     <body style = "background-image: url('/fotos/<?php echo $plaatje; ?>');">
