c3f8743793037d9e233f2390f68f68dbcbec4fea
Seantheman01 <sean.v.g@outlook.com>
2023-05-22 10:30:09 +0200

    committed

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    echo date("h:i:s")
-?>
+<!DOCTYPE html>
+<head>
+</head>
+    <body>
+        <style>
+        style {
+            background-image: url('D:/School/Software_developer/PHP/fotos/morning.png');
+        }
+        </style>
+    </body>
+</html>
