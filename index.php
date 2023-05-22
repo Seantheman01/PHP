bd09b387e40a4d281ba7412b671e144bdf437be6
Seantheman01 <sean.v.g@outlook.com>
2023-05-22 13:07:00 +0200

    committed

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
     <?php
         date_default_timezone_set("Europe/Amsterdam");
         $tijd = date("h:i:s");
-        $plaatje = "morning.png";
+        $ochtend = "morning.png";
+        $middag = "afternoon.png";
+        $avond = "evening.png";
+        $nacht = "night.png";
     ?>
 }
+
 </style>
-<body style = "background-image: url(/fotos/<?php echo $plaatje; ?>);">
-<h1 style = "text-align: center; margin: 280px; ">Goedemorgen!</h1>
+<body style = "background-image: url(/fotos/<?php echo $ochtend; ?>);">
+<h1 style = "text-align: center; margin: 280px; ">Goede morgen!</h1>
 <h1 style = "text-align: center; margin 280px; " id="tijd">Het is nu <?php echo $tijd; ?></h1>
 </body>
 </html>
