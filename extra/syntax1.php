74ef40e7e5f7a6585bc35b81f6a73c7586516bc2
Seantheman01 <sean.v.g@outlook.com>
2023-05-25 12:54:49 +0200

    committed

--- extra/syntax1.php
+++ extra/syntax1.php
@@ -8,21 +8,19 @@
 function lijst_optellen($numbers) {
     $sum = 0;
 
-    foreach($numbers as $number)
-    if ($number%2==0) {
+    foreach($numbers as $number) {
+        
+        if ($number%2==0) {
+            $sum += $number;
 
-        $sum += $number;
-
+        }
     }
 
     return $sum;
-
 }
 
 $getallenLijst = [1, 2, 3, 4];
-
 $result = lijst_optellen($getallenLijst);
-
 echo $result;
 ?>
 </body>
