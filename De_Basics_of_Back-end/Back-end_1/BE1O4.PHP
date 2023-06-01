a2dbf268eeefaa980eaacdca83e6c4c307f910cb
Seantheman01 <sean.v.g@outlook.com>
2023-06-01 10:58:03 +0200

    committed

--- De_Basics_of_Back-end/Back-end_1/BE1O4.PHP
+++ De_Basics_of_Back-end/Back-end_1/BE1O4.PHP
@@ -3,19 +3,48 @@
 </head>
 <style>
 #tijd {
-    <?php
-        date_default_timezone_set("Europe/Amsterdam");
-        $tijd = date("H:i:s");
-        $ochtend = "morning.png";
-        $middag = "afternoon.png";
-        $avond = "evening.png";
-        $nacht = "night.png";
-    ?>
+    background-size: cover;
+    background-position: center;
+    width: 400px;
+    height: 400px;
+}
+
+<?php
+    date_default_timezone_set("Europe/Amsterdam");
+    $tijd = date("H:i:s");
+?>
+
+h1 {
+    position: absolute;
+    top: 50%;
+    left: 18%;
+}
+
+h2 {
+    position: absolute;
+    top: 80%;
+    left: 45%;
 }
 </style>
 
-<body style = "background-image: url(/fotos/<?php echo $ochtend; ?>);">
-<h1 style = "text-align: center; margin: 280px; ">Goede morgen!</h1>
+<?php 
+if ($tijd >= "06:00:00" && $tijd < "12:00:00") {
+    $background = '<body style = "background-image: url(fotos/morning.png)">';
+    echo '<h2>Goede morgen!</h2>';
+}
+elseif ($tijd >= "12:00:00" && $tijd < "18:00:00") {
+    $background = '<body style = "background-image: url(fotos/afternoon.png)">';
+    echo '<h2>Goede middag!</h2>';
+}
+elseif ($tijd >= "18:00:00" && $tijd < "00:00:00") {
+    $background = '<body style = "background-image: url(fotos/evening.png)">';
+    echo '<h2>Goede avond!</h2>';
+}
+elseif ($tijd >= "00:00:00" && $tijd < "06:00:00") {
+    $background = '<body style = "background-image: url(fotos/night.png)">';
+    echo '<h2>Goede nacht!</h2>';
+}
+echo $background; ?>
 <h1 style = "text-align: center; margin: 280px; " id="tijd">Het is nu <?php echo $tijd; ?></h1>
 </body>
 </html>
