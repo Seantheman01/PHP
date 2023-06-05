2bc366d3b33c5ad742da636122ba4d74529899ea
Seantheman01 <sean.v.g@outlook.com>
2023-06-05 10:21:24 +0200

    commit

---
+++ extra/opdracht.php
@@ -0,0 +1,27 @@
+<?php
+function db_connect(): PDO
+{
+    $host = '127.0.0.1';
+    $db_name = 'database';
+    $db_user = 'username';
+    $db_password = 'password';
+    $db = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    return new PDO($db, $db_user, $db_password, $options);
+}
+
+$db = db_connect();//Assign to variable before use
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
