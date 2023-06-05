d63de50bd64a3544e3667fb640d20c2a7d9939a5
Seantheman01 <sean.v.g@outlook.com>
2023-06-05 11:27:14 +0200

    committed

--- extra/opdracht.php
+++ extra/opdracht.php
@@ -9,4 +9,12 @@ $db_database = 'studentenadmin';
 
 $db = new PDO('mysql:host='.$db_host.'; port=3306; dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$select = $db->prepare("SELECT `column`, `column2` FROM `table`");
+$select->execute();
+while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+    $db_col = $row['column'];
+    $db_col2 = $row['column2'];
+    echo "$db_col $db_col2<br>";
+}
 ?>
