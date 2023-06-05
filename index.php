5627236598a02e97dcd9458b97d1eb89c9d6d061
Seantheman01 <sean.v.g@outlook.com>
2023-06-05 11:45:31 +0200

    committed

--- index.php
+++ index.php
@@ -10,11 +10,12 @@ $db_database = 'studentenadmin';
 $db = new PDO('mysql:host='.$db_host.'; port=3306; dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$select = $db->prepare("SELECT `voornaam`, `achternaam` FROM `student`");
+$select = $db->prepare("SELECT `voornaam`, `achternaam`, geboortedatum FROM `student`");
 $select->execute();
 while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
     $db_col = $row['voornaam'];
     $db_col2 = $row['achternaam'];
-    echo "$db_col $db_col2<br>";
+    $db_col3 = $row['geboortedatum'];
+    echo "$db_col $db_col2 $db_col3<br>";
 }
 ?>
