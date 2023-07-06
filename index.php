a9ccbc57c68291f0c87835c9af9b3a17447e6ffd
Seantheman01 <sean.v.g@outlook.com>
2023-07-06 10:06:01 +0200

    committed

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php echo "<h1>Hello world!</h1>";
+<?php 
+echo "<h1>Hello world!</h1>";
 
 define("HELLO", "<h1>Hello world!</h1>");
 echo HELLO;
@@ -6,6 +7,10 @@ echo HELLO;
 $x = "Learning PHP";
 $y = HELLO;
 echo $x = $y;
+
+$a = "Hello";
+$b = "world!";
+echo "<h1>".$a." ".$b."</h1>";
 
 
 ?>
