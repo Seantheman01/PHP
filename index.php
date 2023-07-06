312005c53cc4fcd2866064dfe06c3c2a6e45871e
Seantheman01 <sean.v.g@outlook.com>
2023-07-06 10:25:02 +0200

    committed

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ echo $getal1 ." + ".  $getal2 ." = ". $getal1 + $getal2 ."<br>";
 echo $getal1 ." - ".  $getal2 ." = ". $getal1 - $getal2 ."<br>";
 echo $getal1 ." x ".  $getal2 ." = ". $getal1 * $getal2 ."<br>";
 echo $getal1 ." : ".  $getal2 ." = ". $getal1 / $getal2 ."<br>";
+echo "<br>";
 
 for ($x = 0; $x <= 10; $x++) {
     echo "6 x ".$x." = ". 6*$x."<br>";
