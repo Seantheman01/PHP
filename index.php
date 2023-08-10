d769069ea3ebd9e91b97b44d33df8d2faba7f786
Seantheman01 <sean.v.g@outlook.com>
2023-08-10 16:00:58 +0200

    committed

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <h1>De ingevulde gegevens zijn: </h1>
-    <p>Naam: </p> 
-    <p>Email: </p> 
+    <link rel="stylesheet" href="lay-out.css">
+    <form action="welcome.php" method="post">
+        <h1>De ingevulde gegevens zijn: </h1>
+        <label for="naam">Naam:</label><input></input><br>
+        <label for="email">Email:</label><input></input><br>
+        <input type="submit" value="Submit">
+    </form> 
 </body>
 </html>
