5e894a9fc0a2e57a06dc75aa61ca393104659812
Seantheman01 <sean.v.g@outlook.com>
2023-08-15 14:37:35 +0200

    committed#

--- De_Basics_of_Back-end/Back-end_2/welcome_POST.php
+++ De_Basics_of_Back-end/Back-end_2/welcome_POST.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <body>
     <h1>De ingevulde gegevens zijn: </h1>
-    <p>Naam: <?php echo $_POST['naam']?> </p>
-    <p>Emailadres: <?php echo $_POST['email']?> </p> 
+    Naam: <?php echo $_POST['naam']?>
+    Emailadres: <?php echo $_POST['email']?>
 </body>
 </html>
