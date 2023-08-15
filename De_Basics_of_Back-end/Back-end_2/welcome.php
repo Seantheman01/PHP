a5ffc46932e04f03827a8fc123b75febab3f46c3
Seantheman01 <sean.v.g@outlook.com>
2023-08-15 13:44:07 +0200

    committed

--- De_Basics_of_Back-end/Back-end_2/welcome.php
+++ De_Basics_of_Back-end/Back-end_2/welcome.php
@@ -2,13 +2,6 @@
 <head>
 </head>
 <body>
-    <h1>De ingevulde gegevens zijn: </h1>
-    <p>Naam: </p> 
-    <p>Email: </p> 
-<?php
-echo "De ingevulde gegevens zijn: ";
-echo "Naam: "$_GET['subject'];
-echo "Email: "$_GET['subject'];
-?>
+    
 </body>
 </html>
