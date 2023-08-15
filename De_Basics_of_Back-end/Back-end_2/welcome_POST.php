90cb6517b976188e38998977ab7ba3db145b1b98
Seantheman01 <sean.v.g@outlook.com>
2023-08-15 14:14:04 +0200

    committed

---
+++ De_Basics_of_Back-end/Back-end_2/welcome_POST.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <h1>De ingevulde gegevens zijn: </h1>
+    <p>Naam: <?php echo $_GET['naam']?> </p>
+    <p>Emailadres: <?php echo $_GET['email']?> </p> 
+</body>
+</html>
