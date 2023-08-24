72295c0670f72e0a9dded2917fd82ff44d812e19
Seantheman01 <sean.v.g@outlook.com>
2023-08-24 16:18:15 +0200

    committed

--- De_Basics_of_Back-end/Back-end_2/welcome.php
+++ De_Basics_of_Back-end/Back-end_2/welcome.php
@@ -6,6 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        
+    ?>
+
+    <link rel="stylesheet" href="lay-out.css">
+    <form action="welcome.php" method="get">
+        Naam: <input type="text" name="naam"><br>
+        Email: <input type="text" name="email"><br>
+        <div class="button">
+            <button type="submit">Verzenden</button>
+        </div>
+    </form>
 </body>
 </html>
