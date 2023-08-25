cadd87308d57baacaf13841984bc4a29fc3ce7a7
Seantheman01 <sean.v.g@outlook.com>
2023-08-25 20:56:57 +0200

    committed

--- De_Basics_of_Back-end/Back-end_2/welcome.php
+++ De_Basics_of_Back-end/Back-end_2/welcome.php
@@ -6,11 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="post">  
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
+    <link rel="stylesheet" href="lay-out.css">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+        $nameErr = "Vul uw naam in";
+        } else {
+        $name = test_input($_POST["name"]);
+        }
+        if (empty($_POST["email"])) {
+            $emailErr = "Vul uw email in";
+        } else {
+        $email = test_input($_POST["email"]);
+        }
+    }
+    ?>
+    <p><span class="error">* Vul iets in</span></p>
+    <form method="post"> 
+    Name: <input type="text" name="name">
+    <span class="error">* </span><br>
+    E-mail: <input type="text" name="email">
+    <span class="error">* </span><br>
     <div class="button">
-            <button type="submit">Verzenden</button>
+        <button type="submit">Verzenden</button>
     </div>
 </body>
 </html>
